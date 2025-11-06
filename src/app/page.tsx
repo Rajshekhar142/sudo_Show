26df05feb3829e13190b997daa999e6029dd9a80
Rajshekhar Mishra <rajsheakharmishra001@gmail.com>
2025-11-06 19:24:37 +0530

    should break: if ci is working by passing locally--no-verfiy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 export default function Home() {
   const ur = Math.random();
+  const anotherone = ur * 2;
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col items-center gap-8">
