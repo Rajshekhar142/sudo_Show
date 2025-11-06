ebdd2dc8d392557529055422a3e3720b113c99f3
Rajshekhar Mishra <rajsheakharmishra001@gmail.com>
2025-11-06 19:16:58 +0530

    ci checks: unused Var: bypassing husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 export default function Home() {
+  const ur = Math.random();
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col items-center gap-8">
